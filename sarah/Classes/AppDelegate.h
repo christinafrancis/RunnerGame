240776729049a094b18a41242fd39124a0dfbc50
Christina Francis <christina@unknown881fa11d5346.gateway.2wire.net>
2014-01-22 11:54:26 -0800

    Initial Commit

---
+++ sarah/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  sarah
+//
+//  Created by Christina Francis on 1/22/14.
+//  Copyright Christina Francis 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ sarah/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sarah
+//
+//  Created by Christina Francis on 1/22/14.
+//  Copyright Christina Francis 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
